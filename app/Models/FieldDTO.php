992b190634ae7337a16a760978b08d1bf736d21c
angtdiaz <nicoltuarezd@gmail.com>
2023-11-11 05:08:14 -0500

    feat: add image to fieldDTO

--- app/Models/FieldDTO.php
+++ app/Models/FieldDTO.php
@@ -8,10 +8,12 @@ class FieldDTO
 
     public int $id;
     public String $name;
+    public ?String $image;
 
     public function __construct($field)
     {
         $this->id = $field['id'];
         $this->name = $field['field'];
+        $this->image  = $field['image'] ?? null;
     }
 }
