1412d10717e2e22389953f10263a8639b96ba33b
angtdiaz <nicoltuarezd@gmail.com>
2023-11-11 05:09:44 -0500

    docs: docs

--- app/Traits/DigimonTrait.php
+++ app/Traits/DigimonTrait.php
@@ -5,10 +5,20 @@ namespace App\Traits;
 use Illuminate\Pagination\LengthAwarePaginator;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Log;
 
 trait DigimonTrait
 {
-    public function getDigimonList($page, $pageSize)
+    /**
+     * Retrieves a list of Digimon from the Digi-API based on the given parameters.
+     *
+     * @param int $page The page number to retrieve.
+     * @param int $pageSize The number of Digimon to retrieve per page.
+     * @param string $name The name of the Digimon to search for.
+     *
+     * @return \Illuminate\Pagination\LengthAwarePaginator The paginated list of Digimon.
+     */
+    public function getDigimonList($page, $pageSize, $name)
     {
         $client = new Client();
         try {
@@ -16,12 +26,13 @@ trait DigimonTrait
                 'query' => [
                     'page' => $page,
                     'pageSize' => $pageSize,
+                    'name' => $name
                 ]
             ]);
             $data = json_decode($response->getBody(), true);
             if (!isset($data['content'])) {
                 Log::error("Respuesta inesperada de la API: " . json_encode($data));
-                return null;
+                return [];
             }
             $content = $data['content'];
             $paginator = new LengthAwarePaginator(
@@ -34,10 +45,17 @@ trait DigimonTrait
             return $paginator;
         } catch (GuzzleException $e) {
             Log::error("Error al obtener datos de la API: " . $e->getMessage());
-            return null;
+            return [];
         }
     }
 
+    /**
+     * Retrieves a Digimon by its ID from the Digi-API.
+     *
+     * @param int $id The ID of the Digimon to retrieve.
+     *
+     * @return array|null Returns an array with the Digimon data if successful, or null if an error occurred.
+     */
     public function getDigimonById($id)
     {
         $client = new Client();
